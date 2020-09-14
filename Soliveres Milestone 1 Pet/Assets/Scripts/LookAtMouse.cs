c1c9373b2e5f894dfae08ee544b3de4a00c65bad
Ro Soliveres <roumaine.soliveres@gmail.com>
2020-09-14 16:07:33 +0800

    OTG003 Roumaine Soliveres Milestone 1 Pet

---
+++ Soliveres Milestone 1 Pet/Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour
+{
+    float rotSpeed = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookAtMouse = new Vector3(Input.mousePosition.x,
+                                        this.transform.position.y,
+                                        Input.mousePosition.z);
+
+        Vector3 direction = lookAtMouse - transform.position;
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
+                                                Quaternion.LookRotation(direction),
+                                                Time.deltaTime * rotSpeed);
+
+    }
+}
---
+++ Soliveres Milestone 1 Pet/Assets/Scripts/TravelForwardToHooman.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TravelForwardToHooman : MonoBehaviour
+{
+    public Transform hooman;
+    public float speed  = 5;
+    public float rotSpeed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookAtHooman = new Vector3(hooman.position.x,
+                                            this.transform.position.y,
+                                            hooman.position.z);
+
+        Vector3 direction = lookAtHooman - transform.position;
+
+        
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                                                    Quaternion.LookRotation(direction),
+                                                    Time.deltaTime * rotSpeed);
+        
+
+        //this.transform.position = Vector3.Slerp(lookAtHooman, transform.position, Time.deltaTime * rotSpeed);
+
+        
+        if(Vector3.Distance(lookAtHooman, transform.position) > 1)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        
+    }
+}
