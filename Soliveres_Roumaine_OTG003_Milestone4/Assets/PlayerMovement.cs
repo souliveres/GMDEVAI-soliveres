b2edc122d6edf40dbf1c25606c8abb3b8d239365
Ro Soliveres <roumaine.soliveres@gmail.com>
2020-10-01 17:56:33 +0800

    Commit of Milestone 4 Oct 1

---
+++ Soliveres_Roumaine_OTG003_Milestone4/Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            // Go left
+            // Below works because multipling a vector by a scalar
+            // Changes the vector's MAG but NOT DIR
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            // Go right
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            // Go forwards
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            // Go backwards
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Soliveres_Roumaine_OTG003_Milestone4/Assets/TravelForwardToPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TravelForwardToPlayer : MonoBehaviour
+{
+    public Transform player;
+    public float speed  = 5;
+    public float rotSpeed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 lookAtPlayer = new Vector3(player.position.x,
+                                            this.transform.position.y,
+                                            player.position.z);
+
+        Vector3 direction = lookAtPlayer - transform.position;
+
+        
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                                                    Quaternion.LookRotation(direction),
+                                                    Time.deltaTime * rotSpeed);
+        
+
+        
+        if(Vector3.Distance(lookAtPlayer, transform.position) > 1)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+    }
+}
